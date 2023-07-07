6857637d51d7aae0aef391e79960c5e59af6e4ab
ywyi1992 <ywyi1992@gmail.com>
2023-07-07 15:14:32 +0900

    redis module add

--- nestjs/toy-project/src/main.ts
+++ nestjs/toy-project/src/main.ts
@@ -3,16 +3,29 @@ import { AppModule } from "./app.module";
 import { INestApplication } from "@nestjs/common";
 import swaggerInit from "./modules/swagger";
 import { errorPrint, getError } from "./util/Error";
+import { RedisManager, RedisVo } from "./modules/catch/redis";
 
-const port = 3000;
-
+const port = 3001;
 (() => {
     NestFactory
     .create(AppModule)
     .then((app: INestApplication<any>) => {
         const initCommandList: string[] = [];
+        RedisManager
+        .getInstance()
+        .then(async (redis: RedisVo) => {
+            try {
+                console.log(await redis.get("test"));
+                await redis.close();
+            } catch(e) {
+                throw e;
+            }
+        })
+        .catch(e => {
+            throw e;
+        })
         const swaggerInitResult = swaggerInit(app);
-        initCommandList.push(`swagger init: ${swaggerInitResult}`)
+        initCommandList.push(`swagger init: ${swaggerInitResult}`);
         app
         .listen(port)
         .then(() => {

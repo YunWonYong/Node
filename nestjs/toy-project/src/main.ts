cedd1e52cf57db43df7f7565d58276a480c1dffe
ywyi1992 <ywyi1992@gmail.com>
2023-08-26 15:07:49 +0900

    cors code add

--- nestjs/toy-project/src/main.ts
+++ nestjs/toy-project/src/main.ts
@@ -13,6 +13,10 @@ import SwaggerDocumentBuilder from "./common/swagger";
     const swaggerConfig = config.getSwaggerConfig();
 
     new SwaggerDocumentBuilder(swaggerConfig).build(nestFactory, appConfig.env);
-    
+    nestFactory.enableCors({
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        origin: true,
+        credentials: true
+    });
     await nestFactory.listen(appConfig.port);
 })();
